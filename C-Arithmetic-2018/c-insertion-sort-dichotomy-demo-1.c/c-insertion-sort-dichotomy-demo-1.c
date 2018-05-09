0e99ab52254095b8bf3057a6b88a854be8257fb5
asm32net <379754432@qq.com>
2018-05-10 02:27:04 +0800

    c-insertion-sort-dichotomy-demo-1.c ... add

---
+++ C-Arithmetic-2018/c-insertion-sort-dichotomy-demo-1.c/c-insertion-sort-dichotomy-demo-1.c
@@ -0,0 +1,47 @@
+// c-insertion-sort-dichotomy-demo-1.c
+#include <stdio.h>
+
+void PA_DisplayData(int data[], int nCount){
+    for(int i = 0; i < nCount; i++){
+        if(i) printf(", ");
+        printf("%d", data[i]);
+    }
+    printf("\n");
+}
+
+void InsertionSortDichotomy(int A[], int n){
+    for(int i = 1; i < n; i++){
+        int get = A[i];
+        int left = 0;
+        int right = i - 1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(A[mid] > get){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        for(int j = i - 1; j >= left; j--){
+            A[j + 1] = A[j];
+        }
+        A[left] = get;
+    }
+}
+
+void main(){
+    // int _source[] = {41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36};
+    int _source[] = {76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82};
+    int nCount = sizeof(_source) / sizeof(int);
+
+    printf("Item count: %d\n", nCount);
+
+    // srand(time(NULL));
+    // for(int i = 0; i < 20; i++){
+    //     _source[i] = rand() % 100;
+    // }
+
+    PA_DisplayData(_source, nCount);
+    InsertionSortDichotomy(_source, nCount);
+    PA_DisplayData(_source, nCount);
+}
