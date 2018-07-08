413abf185f0090dba2c36b24c2f93aa6cea274b7
asm32cn <asm32@qq.com>
2018-07-08 11:38:43 +0800

    frame.cpp ... modify

--- C-Template/frame.cpp
+++ C-Template/frame.cpp
@@ -11,14 +11,14 @@ int WINAPI WinMain(HINSTANCE hThisInst,HINSTANCE hPrevInst,
 	WNDCLASS wc1;
 
 	//定义一个窗口类
-	wc1.hInstance=hThisInst;		//改实例的句柄
-	wc1.lpszClassName=szWinName;	//窗口类的名称
-	wc1.lpfnWndProc=WindowFunc;		//窗口名称
-	wc1.style=0;					//默认模式
+	wc1.hInstance=hThisInst;        //改实例的句柄
+	wc1.lpszClassName=szWinName;    //窗口类的名称
+	wc1.lpfnWndProc=WindowFunc;     //窗口名称
+	wc1.style=0;                    //默认模式
 
-	wc1.hIcon=LoadIcon(NULL,IDI_APPLICATION);	//图标模式
-	wc1.hCursor=LoadCursor(NULL,IDC_ARROW);		//光标模式
-	wc1.lpszMenuName=NULL;						//没有菜单
+	wc1.hIcon=LoadIcon(NULL,IDI_APPLICATION);   //图标模式
+	wc1.hCursor=LoadCursor(NULL,IDC_ARROW);     //光标模式
+	wc1.lpszMenuName=NULL;                      //没有菜单
 
 	wc1.cbClsExtra=0;
 	wc1.cbWndExtra=0;
@@ -31,16 +31,16 @@ int WINAPI WinMain(HINSTANCE hThisInst,HINSTANCE hPrevInst,
 
 	//创建一个窗口
 	hwnd=CreateWindow(
-		szWinName,						//窗口类名
-		"Windows 98 Framework",			//标题
-		WS_OVERLAPPEDWINDOW,			//窗口模式
-		CW_USEDEFAULT,					// X 坐标
-		CW_USEDEFAULT,					// Y 坐标
-		CW_USEDEFAULT,					//宽度
-		CW_USEDEFAULT,					//高度
-		HWND_DESKTOP,					//没有父窗口
-		NULL,							//没有菜单
-		hThisInst,						//该程序实例的句柄
+		szWinName,              //窗口类名
+		"Windows 98 Framework", //标题
+		WS_OVERLAPPEDWINDOW,    //窗口模式
+		CW_USEDEFAULT,          // X 坐标
+		CW_USEDEFAULT,          // Y 坐标
+		CW_USEDEFAULT,          //宽度
+		CW_USEDEFAULT,          //高度
+		HWND_DESKTOP,           //没有父窗口
+		NULL,                   //没有菜单
+		hThisInst,              //该程序实例的句柄
 		NULL
 		);
 
@@ -50,8 +50,8 @@ int WINAPI WinMain(HINSTANCE hThisInst,HINSTANCE hPrevInst,
 
 	//创建消息循环
 	while(GetMessage(&msg,NULL,0,0)){
-		TranslateMessage(&msg);			//允许使用键盘
-		DispatchMessage(&msg);			//返回窗口控制
+		TranslateMessage(&msg); //允许使用键盘
+		DispatchMessage(&msg);  //返回窗口控制
 	}
 	return msg.wParam;
 }
