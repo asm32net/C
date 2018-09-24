1ce4217e4ddfd213ae3ed2d584461096b7242293
asm32net <379754432@qq.com>
2018-09-24 13:22:09 +0800

    resource.h ... add

---
+++ C-Utils/C-Utils-2018/c-click-demo-1.c/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by c-click-demo-1-2.rc
+//
+#define IDOK                            1
+#define IDCANCEL                        2
+#define SUBLANG_CHINESE_SIMPLIFIED      0x02
+#define IDC_START                       3
+#define LANG_CHINESE                    0x04
+#define IDD_DIALOG1                     101
+#define IDC_PROGRESS_COUNT              1000
+#define IDC_PROGRESS_STEP               1001
+#define IDC_STEP                        1003
+#define IDC_POSOTION_X                  1004
+#define IDC_POSOTION_Y                  1005
+#define IDC_COUNT                       1006
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
